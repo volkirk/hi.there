f6b518d7f057fffa0388a9c2131e666e24942f4b
volkirk <yourgoodd@gmail.com>
2020-12-02 20:50:11 +0800

    'third'

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+var img = document.createElement("IMG")
 var name=prompt('Введите ваше имя.');
-alert(name + ' хорошего дня!');
+// img.src = 'svetik.jpeg'
+
+// var pic = document.createElement("IMG");
+// pic.src = "web\module 8\rjomba\svetik.jpeg";
+// pc.appendChild(pic);
+
+if (name=='Света'){
+    alert(name + ' Лучшая малышка на свете!');
+var pc = document.getElementById("pic_cntr");
+pc.innerHTML="<img src='svetik.jpeg'>";
+}else{
+    alert('Выйди от сюда');
+}
