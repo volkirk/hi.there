8e6ba2cffffef4282a8e98643f6a33dab0b24efe
volkirk <yourgoodd@gmail.com>
2020-12-02 20:56:52 +0800

    'correction'

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 var img = document.createElement("IMG")
 var name=prompt('Введите ваше имя.');
-// img.src = 'svetik.jpeg'
-
-// var pic = document.createElement("IMG");
-// pic.src = "web\module 8\rjomba\svetik.jpeg";
-// pc.appendChild(pic);
-
 if (name=='Света'){
     alert(name + ' Лучшая малышка на свете!');
 var pc = document.getElementById("pic_cntr");
