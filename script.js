0b2dfbf908ad6f61ed1a50333b09b8f442aeac8b
volkirk <73282674+volkirk@users.noreply.github.com>
2021-01-13 02:37:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var img = document.createElement("IMG")
 var name=prompt('Полина, введите ответ.');
-if (name=='lets go'){
+if (name=='lets go'| name=='Let`s go'| name =='let`s go'|name=='Lets go'){
     alert('Поздравляю! Lets go покупать перчатки!');
 var pc = document.getElementById("pic_cntr");
 pc.innerHTML="<img src='bd.gif'>";
