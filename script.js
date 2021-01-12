817a8ae55a9d4fb8e8afdd072f4448d61e6cf29c
volkirk <73282674+volkirk@users.noreply.github.com>
2021-01-13 02:07:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var img = document.createElement("IMG")
 var name=prompt('Полина, введите ответ.');
 if (name=='lets go'){
     alert(name + 'Поздравляю! Lets go покупать перчатки!');
-// var pc = document.getElementById("pic_cntr");
-// pc.innerHTML="<img src='svetik.jpeg'>";
+var pc = document.getElementById("pic_cntr");
+pc.innerHTML="<img src='bd.gif'>";
 }else{
     alert('попробуй еще раз... что то не верно.');
 }
