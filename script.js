973fda03155228f7b85bdae389d4284ef7f30e40
unknown <@dn.ti.local>
2021-07-21 18:18:59 +0800

    -roses

---
+++ script.js
@@ -0,0 +1,11 @@
+var img = document.createElement("IMG")
+var name=prompt('введите свое имя.');
+if (name=='Таня'|name=='Татьяна'){
+    alert(`Поздравляю! Вот Твой приз! (●'◡'●)`);
+// if (name=='lets go'| name=='Let`s go'| name =='let`s go'|name=='Lets go'){
+//     alert('Поздравляю! Lets go покупать перчатки!');
+var pc = document.getElementById("pic_cntr");
+pc.innerHTML="<img src='roses.jpg'>";
+}else{
+    alert('попробуй еще раз... что то не верно..');
+}
