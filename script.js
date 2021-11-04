2d10bad0f0914cb48c146ca14839cec4dc818f46
volkirk <73282674+volkirk@users.noreply.github.com>
2021-11-05 06:44:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var img = document.createElement("IMG")
-var name=prompt('Полина, введите ответ.');
-if (name=='lets go'| name=='Let`s go'| name =='let`s go'|name=='Lets go'){
-    alert('Поздравляю! Lets go покупать перчатки!');
+var name=prompt(" введите ответ.');
+if (name=='Маруся'| name=='Маша'| name =='маруся'|name=='маша'){
+    alert('Поздравляю! Поцелуй Сентуза по французки!');
 var pc = document.getElementById("pic_cntr");
 pc.innerHTML="<img src='bd.gif'>";
 }else{
