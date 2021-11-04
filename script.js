f89f96eddc510657d09f60049948410a13c9dc4e
volkirk <73282674+volkirk@users.noreply.github.com>
2021-11-05 06:50:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var img = document.createElement("IMG")
 var name=prompt(" введите ответ.');
 if (name=='Маруся'| name=='Маша'| name =='маруся'|name=='маша'){
     alert('Поздравляю! Поцелуй Сентуза по французки!');
-var pc = document.getElementById("pic_cntr");
+// var pc = document.getElementById("pic_cntr");
 pc.innerHTML="<img src='bd.gif'>";
 }else{
     alert('попробуй еще раз... что то не верно..');
