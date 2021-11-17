edf0cd4a007f31ca2db2c4118d9a37c81bb8cd21
volkirk <73282674+volkirk@users.noreply.github.com>
2021-11-17 12:47:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var img = document.createElement("IMG")
-var name=prompt(" введите ответ.');
-if (name=='Маруся'| name=='Маша'| name =='маруся'|name=='маша'){
-    alert('Поздравляю! Поцелуй Сентуза по французки!');
-// var pc = document.getElementById("pic_cntr");
-pc.innerHTML="<img src='bd.gif'>";
-}else{
-    alert('попробуй еще раз... что то не верно..');
-}
+// var img = document.createElement("IMG")
+// var name=prompt(" введите ответ.');
+// if (name=='Маруся'| name=='Маша'| name =='маруся'|name=='маша'){
+//     alert('Поздравляю! Поцелуй Сентуза по французки!');
+// // var pc = document.getElementById("pic_cntr");
+// pc.innerHTML="<img src='bd.gif'>";
+// }else{
+//     alert('попробуй еще раз... что то не верно..');
+// }
