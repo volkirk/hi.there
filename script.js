c5ec8237fc78e684dd38c1c1c9f6686a7ebfc03b
volkirk <73282674+volkirk@users.noreply.github.com>
2021-11-17 12:49:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var img = document.createElement("IMG")
 var name=prompt('введите свое имя.');
-if (name=='Таня'|name=='Татьяна'){
-    alert(`Поздравляю! Вот Твой приз! (●'◡'●)`);
+if (name=='Маруся'|name=='Маша'|name=='маша'|name=='маруся'){
+    alert(`Поздравляю! Можешь чмокнуть Сентуза! (●'◡'●)`);
 // if (name=='lets go'| name=='Let`s go'| name =='let`s go'|name=='Lets go'){
 //     alert('Поздравляю! Lets go покупать перчатки!');
 var pc = document.getElementById("pic_cntr");
